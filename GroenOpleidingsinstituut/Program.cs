cad0510e59fd950d88633298c029db7e38431932
Arthur Schut <aschutgh@gmail.com>
2018-06-05 21:03:44 +0200

    Add project files.

---
+++ GroenOpleidingsinstituut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroenOpleidingsinstituut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
